7ef8b4e03a4cefd3cadb6a5fa95dab043e54f5a7
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-07-30 09:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
 
+document.addEventListener("DOMContentLoaded", () => {
+  const newTodoInput = document.getElementById("newTodoInput");
+  const addTodoBtn = document.getElementById("addTodoBtn");
+  const todoList = document.getElementById("todoList");
+
+  addTodoBtn.addEventListener("click", () => {
+    const todoText = newTodoInput.value.trim(); // Remove leading/trailing spaces
+    if (todoText !== "") {
+      const todoItem = document.createElement("li");
+      todoItem.textContent = todoText;
+      todoList.appendChild(todoItem);
+      newTodoInput.value = ""; // Clear the inaput after adding the todo
+    }
+  });
+
+  newTodoInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTodoBtn.click(); // Simulate a click on the "Add todo" button when Enter key is pressed
+    }
+  });
